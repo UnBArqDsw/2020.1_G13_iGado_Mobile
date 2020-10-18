50f99b6c3b5aac7d5304b0e4931153a85b5ed8bb
guilherme-mendes <guimendesp12@gmail.com>
2020-10-18 15:38:02 -0300

    UnBArqDsw/2020.1_G13_Projeto#102 📦 NEW: Add routes services to integration backend users
    Co-authored-by: Iuri Severo <nobreiuri@gmail.com>

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:igado_front/components/icon_text_form_field.dart';
 import 'package:igado_front/constants.dart';
+import 'package:igado_front/services/user_service.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -8,70 +9,94 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  Future<User> futureUser;
+
+  @override
+  void initState() {
+    super.initState();
+    futureUser = UserService().fetchUser(1);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kBrown2,
-        automaticallyImplyLeading: false,
-      ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        decoration: kBackgroundTheme,
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Perfil',
-                style: TextStyle(
-                    color: kBrown2,
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 35, 10, 10),
+    return FutureBuilder<User>(
+      future: futureUser,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          User userInfo = snapshot.data;
+          print(userInfo);
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: kBrown2,
+              automaticallyImplyLeading: false,
+            ),
+            body: Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: kBackgroundTheme,
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 40.0, vertical: 10.0),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Image(
-                      image: AssetImage('assets/images/profile.png'),
-                      height: 150.0,
+                    Text(
+                      'Perfil',
+                      style: TextStyle(
+                          color: kBrown2,
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold),
                     ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text('Guilherme Mendes',
-                        style: TextStyle(color: kBrown1, fontSize: 26.0)),
-                    Text('Proprietario da fazenda x',
-                        style: TextStyle(color: kBrown1, fontSize: 20.0)),
-                    Text('guilherme@guilherme.com',
-                        style: TextStyle(color: kBrown1, fontSize: 15.0)),
-                    SizedBox(
-                      height: 115.0,
-                    ),
-                    Center(
-                      child: FlatButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Sair da Conta',
-                          style: TextStyle(
-                            color: Colors.white,
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 35, 10, 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image(
+                            image: AssetImage('assets/images/profile.png'),
+                            height: 150.0,
                           ),
-                        ),
-                        color: kBrown2,
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(userInfo.fullname,
+                              style: TextStyle(color: kBrown1, fontSize: 25.0)),
+                          Text(
+                              userInfo.isProprietary
+                                  ? 'Proprietário da fazenda ${userInfo.farms[0].toString()}'
+                                  : 'Funcionário da fazenda ${userInfo.farms[0].toString()}',
+                              style: TextStyle(color: kBrown1, fontSize: 20.0)),
+                          Text(userInfo.email,
+                              style: TextStyle(color: kBrown1, fontSize: 15.0)),
+                          SizedBox(
+                            height: 115.0,
+                          ),
+                          Center(
+                            child: FlatButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Sair da Conta',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              color: kBrown2,
+                            ),
+                          )
+                        ],
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        return CircularProgressIndicator();
+      },
     );
   }
 }
