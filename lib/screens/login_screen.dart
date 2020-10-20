d6b2603534f05a6023268cdfefc9752154f61125
Caio Fernandes <caiofernandes@Caios-MacBook-Pro.local>
2020-10-20 11:12:02 -0300

    UnBArqDsw/2020.1_G13_Projeto#102 📦 NEW: Add login integration with backend
    Co-authored-by: Lucas Fellipe <lucasfcm9@gmail.com>

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:igado_front/components/icon_text_form_field.dart';
 import 'package:igado_front/constants.dart';
+import 'package:igado_front/services/login_service.dart';
+
+LoginService loginService = new LoginService();
 
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
+Map<String, String> data;
+var response;
+Map<String, dynamic> formResponse = {
+  "email": "",
+  "password": "",
+};
+
 class _LoginPageState extends State<LoginPage> {
+  Function changeDictData(String field) {
+    return (text) {
+      setState(() {
+        formResponse[field] = text;
+      });
+    };
+  }
+
+  bool checkIfFormResponse(formResponse) {
+    if (formResponse["email"].isEmpty || formResponse["password"].isEmpty)
+      return false;
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,15 +60,34 @@ class _LoginPageState extends State<LoginPage> {
                       icon: Icons.mail_outline,
                       placeholder: "Digite seu e-mail",
                       obscureText: false,
+                      onChange: changeDictData('email'),
                     ),
                     IconTextFormField(
                       title: "Senha",
                       icon: Icons.lock_outline,
                       placeholder: "Digite sua senha",
                       obscureText: true,
+                      onChange: changeDictData('password'),
                     ),
                     FlatButton(
-                      onPressed: () {},
+                      onPressed: checkIfFormResponse(formResponse)
+                          ? () {
+                              setState(() {
+                                data = {
+                                  "email": formResponse['email'],
+                                  "password": formResponse['password'],
+                                };
+                                response = loginService.login(data);
+                                response
+                                    .then((value) => {
+                                          Navigator.pushNamed(
+                                              context, '/profile')
+                                        })
+                                    .catchError(() => {print('deu ruim')});
+                                print('kk');
+                              });
+                            }
+                          : null,
                       child: Text(
                         'Entrar',
                         style: TextStyle(
