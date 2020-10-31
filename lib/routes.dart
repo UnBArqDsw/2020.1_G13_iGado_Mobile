8f2b1395e6424562df288125949de769a5ed3a60
guilherme-mendes <guimendesp12@gmail.com>
2020-10-31 11:22:56 -0300

    UnBArqDsw/2020.1_G13_Projeto#131 📦 NEW: Implements route of the bovines create screen
    Co-authored-by: Lucas Fellipe <lucasfcm9@gmail.com>

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,10 +3,12 @@ import 'package:igado_front/screens/register_screen.dart';
 import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/profile_screen.dart';
+import 'screens/bovine_screen.dart';
 
 Map<String, WidgetBuilder> kRoutes = {
   '/': (context) => SplashScreen(),
   '/login': (context) => LoginPage(),
   '/register': (context) => RegisterScreen(),
   '/profile': (context) => ProfileScreen(),
+  '/bovine': (context) => BovineScreen(),
 };
